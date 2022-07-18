8d55d9d5cab01a7ddfb5f8d6de675850dcad08be
samaosage <samaosage@gmail.com>
2022-07-18 01:02:13 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
-/**
- * print_most_numbers - prints digit 0-9
- */
-void print_most_numbers(void)
-{
-	int i;
 
-	for (i = 0; i <= 9; i++)
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i);
-		}
+		_putchar(a);
 	}
 	_putchar('\n');
 }
